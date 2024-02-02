a209632595ed29972e23c1cbc2a1003a50f2b9b0
soheilsayahvarg <soheilsayahvarg@gmail.com>
2024-02-02 09:53:09 +0330

    fix log and log -n

--- header/prototypes.h
+++ header/prototypes.h
@@ -32,6 +32,7 @@ int run_replace_message(int argc, char *argv[]);
 int run_remove_message(int argc, char *argv[]);
 
 int run_log(int argc, char *argv[]);
+int show_commit_data(int number_of_commit);
 
 int run_branch(int argc, char *argv[]);
 int add_branch(char branch_name[], char neogit_dir_address[]);
